922a2defcc7ac6cd298f2f60329a0353e2bfe6d7
dawidK5 <19233116@studentmail.ul.ie>
2022-03-29 12:17:12 +0100

    Update php

--- basic_functions.php
+++ basic_functions.php
@@ -1,35 +1,69 @@
 <?php
+
 function setupMySQL()
+
 {
-	$server_name = "sql212.epizy.com ";
+
+	$server_name = "sql212.epizy.com";
+
 	$db_username = "epiz_31170569";
+
 	$db_password ="QEvjZHe1Gew";
+
 	$db_name = "epiz_31170569_datingsite";
+
 	$conn = new mysqli($server_name, $db_username, $db_password, $db_name);
+
 	if ($conn->connection_error)
+
 	{
+
 		die("Connection failed");
+
 		exit();
+
 	}
+
 	echo "Connected to DB successfully";
+
 	return $conn;
+
 }
+
+
 
 function isLoggedIn()
+
 {
+
 	return ($_SESSION['logged_in'] && $_SESSION['user_id']);
+
 }
 
+
+
 function wasPosted($arr_of_props)
+
 {
+
 	$posted = isset($_POST);
+
 	if ($posted)
+
 	{
+
 		foreach ($arr_of_props as $posted_prop)
+
 		{
+
 			$posted = $posted && $_POST[$posted_prop];
+
 		}
+
 	}
+
 	return $posted;
+
 }
+
 ?>
