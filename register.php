220a0b9d299a04158ef7ce46b483375376ee9ea9
dawidK5 <19233116@studentmail.ul.ie>
2022-04-25 20:28:17 +0100

    Update registration

--- register.php
+++ register.php
@@ -12,49 +12,73 @@ if (wasPosted(array('email', 'password', 'first_name', 'surname', 'date_of_birth
     $description = $conn -> real_escape_string($_POST['description']);
     $hash_password = md5( $hash_password);
 
-    // Insert into Users
-    $sql_1 = "INSERT INTO Users(firstName, surname, dateOfBirth, gender, genderPreference, location, description) VALUES('{$first_name}', '{$surname}', '{$_POST['date_of_birth']}', '{$_POST['gender']}', '{$_POST['gender_preference']}', '{$_POST['location']}', '{$description}');";
-    $result_1 = $conn->query($sql_1);
-    if (!$result_1) {
-        echo "INSERT error: " . $conn->error;
+    // Check if not registered already
+    $email = $_POST['email'];
+    $sql_0 = "SELECT * FROM Credentials WHERE email = '{$email}' LIMIT 1;";
+    $result_0 = $conn -> query($sql_0);
+    if (!$result_0) {
+        echo "\nAccessing Credentials error: " . $conn->error;
         $conn->close();
         exit;
-    }
+    } elseif ($result_0->num_rows) {
+        // If there is duplication
+        // Check if not banned
+        $userId = ($result_0->fetch_assoc())['userId'];
+        $sql_5 = "SELECT * FROM BannedStatus WHERE userId = {$userId} AND banStatus > 0 LIMIT 1;";
+        $result_5 = $conn->query($sql_5);
+        if (!$result_5) {
+            echo "\nAccessing BannedStatus error: " . $conn->error;
+        } elseif ($result_5->num_rows) {
+            // If user was banned
+            header("Location: banned.html");
+            $conn->close();
+            exit;
+        }
+        echo "<script>alert(\"An account with email " . $email . " already exists.\nYou will be redirected to login page.\");window.location.href = \"login.html\";</script>";
+        $conn->close();
+    } else {
+        // Insert into Users
+        $sql_1 = "INSERT INTO Users(firstName, surname, dateOfBirth, gender, genderPreference, location, description) VALUES('{$first_name}', '{$surname}', '{$_POST['date_of_birth']}', '{$_POST['gender']}', '{$_POST['gender_preference']}', '{$_POST['location']}', '{$description}');";
+        $result_1 = $conn->query($sql_1);
+        if (!$result_1) {
+            echo "\nINSERT error: " . $conn->error;
+            $conn->close();
+            exit;
+        }
 
-    // Check if it worked
-    $sql_2 = "SELECT userId FROM Users WHERE firstName = '{$first_name}' AND surname = '{$surname}' AND dateOfBirth = '{$_POST['date_of_birth']}' AND gender = '{$_POST['gender']}' AND genderPreference = '{$_POST['gender_preference']}' AND location = '{$_POST['location']}' AND description = '{$description}' LIMIT 1;";
-    $result_2 = $conn->query($sql_2);
-    if ((!$result_2)) {
-        echo "Registration userId error: " . $result_2 -> error;
-        $conn->close();
-        exit;
-    }
-    $user_id = $result_2->fetch_assoc();
-    if (!$user_id) {
-        echo "Registration error, userId not found";
-        $conn->close();
-        exit;
-    }
+        // Check if it worked
+        $sql_2 = "SELECT userId FROM Users WHERE firstName = '{$first_name}' AND surname = '{$surname}' AND dateOfBirth = '{$_POST['date_of_birth']}' AND gender = '{$_POST['gender']}' AND genderPreference = '{$_POST['gender_preference']}' AND location = '{$_POST['location']}' AND description = '{$description}' LIMIT 1;";
+        $result_2 = $conn->query($sql_2);
+        if ((!$result_2)) {
+            echo "\nRegistration userId error: " . $conn->error;
+            $conn->close();
+            exit;
+        }
+        $user_id = $result_2->fetch_assoc()['userId'];
+        if (!$user_id) {
+            echo "\nRegistration error, userId not found";
+            $conn->close();
+            exit;
+        }
 
-    // Insert into Credentials
-    $sql_3 = "INSERT INTO Credentials(email, userId, password, isAdmin) VALUES ('{$_POST['email']}', '{$user_id}', '{$hash_password}', 0);";
-    $result_3 = $conn->query($sql_3);
-    if ((!$result_3)) {
-        echo "Registration credentials error: " . $result_3->error;
+        // Insert into Credentials
+        $sql_3 = "INSERT INTO Credentials(email, userId, password, isAdmin) VALUES ('{$_POST['email']}', CAST('{$user_id}' AS INT), '{$hash_password}', 0);";
+        $result_3 = $conn->query($sql_3);
+        if ((!$result_3)) {
+            echo "\nRegistration credentials error: " . $conn->error;
+            $conn->close();
+            exit;
+        }
+        // Insert into BannedStatus
+        $sql_4 = "INSERT INTO BannedStatus(userId, banStatus) VALUES ('{$user_id}', 0);";
+        $result_4 = $conn->query($sql_4);
+        if ((!$result_4)) {
+            echo "\nRegistration ban status error: " . $conn->error;
+            $conn->close();
+            exit;
+        }
         $conn->close();
-        exit;
+        echo "\n Registration successful";
+        header("Location: login.html");
     }
-    // Insert into BannedStatus
-    $sql_4 = "INSERT INTO BannedStatus(userId, banStatus) VALUES ('{$user_id}', 0);";
-    $result_4 = $conn->query($sql_4);
-    if ((!$result_4)) {
-        echo "Registration ban status error: " . $result_4->error;
-        $conn->close();
-        exit;
-    }
-    $conn->close();
-    echo "\n Registration successful";
-    header("Location: login.php");
 }
-exit;
-?>
