54ea37b68ee04246bdabaa1ebf317287c18d0757
dawidK5 <19233116@studentmail.ul.ie>
2022-04-26 10:01:17 +0100

    Update login, feed, mysqli

--- feed.php
+++ feed.php
@@ -1,34 +1,92 @@
 <?php
 require('basic_functions.php');
-session_start();
-if (isLoggedIn())
-{
-	// load the feed as normal
-}
-else
-{
-	//redirect to login
-	header("Location: login.html");
-	exit;
-}
 
-function showCard($rows)
+function getBestMatchingUsersQuery() {
+	return "SELECT * FROM USERS WHERE gender = '".$_COOKIE['gender_preference']."' AND genderPreference = '".$_COOKIE['gender']."';";
+}
+function getUserByName($search_term) {
+	$sql = "SELECT * FROM USERS WHERE firstName LIKE '%".$search_term."%';";
+}
+// function getGenderMatchingUsers($conn, $user_gender, $preferred_gender)
+// {
+// 	// gets rows with all matching users filtering by gender and gender preference
+// 	$sql = "SELECT * FROM Users WHERE preferredGender = '{$user_gender}' AND gender = '{$preferred_gender}';";
+// 	return $conn->query($sql);
+// }
+function showCards($rows)
 {
 	while ($row = $rows->fetch_assoc())
 	{
 		
-		echo "<div class=\"card col-sm-8 offset-1\">
-					  <img class=\"card-img-top\" src=\"data:image/png;base64," . $row['icon'] . "\" alt=\"Card image\">
-					  <div class=\"card-body\">
-						<h4 class=\"card-title\">" . $row['firstName'] . " " . $row['surname'] . "</h4>
-						<p class=\"card-text\">Some example text.</p>
-						<a href=\"profile?user=" .  $row['userId'] . "\" class=\"btn btn-primary\">See Profile</a>
-					  </div>
-					</div>" . "\n";
+		echo '<div class="feed">
+		<div class="head">
+			<div class="user">
+				<div class="profilepicture">
+					<img src="data:image/png;base64,'.$row['icon'].'" alt="user image">
+				</div>
+				<div class="searchName">
+					<a href="#">
+						<h3>'.$row['firstName'].'</h3>
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="photo">
+		<img src="data:image/png;base64,'.$row['icon'].'" alt="user icon">
+		</div>
+		<div class="action-buttons">
+			<div class="interaction-buttons">
+				<span><i class="uil uil-heart"></i></span>
+				<span><i class="uil uil-thumbs-down"></i></span>
+			</div>
+		</div>
+		<div class="caption">
+			<p>'.$row['description'].'</p>
+		</div>
+	</div>';
 	}
 }
-?>
-<?php
-require('filter_functions.php');
-showCard(getGenderMatchingUsers($_COOKIE['gender'], $_COOKIE['preferredGender']));
+function getCustomFiltersQuery() {
+	$filtered_query = 'SELECT * FROM USERS WHERE ';
+	$addons = 0;
+	if (isset($_POST['showMale']) {
+		$filtered_query = $filtered_query . "gender = 'MALE'";
+		$addons = $addons + 1;
+	}
+	if (isset($_POST['showFemale']) {
+		if ($addons > 0) {
+			$filtered_query = $filtered_query . ' AND ';
+		}
+		$filtered_query = $filtered_query . "gender = 'FEMALE'";
+		$addons = $addons + 1;
+	}
+	if (isset($_POST['showOther']) {
+		if ($addons > 0) {
+			$filtered_query = $filtered_query . ' AND ';
+		}
+		$filtered_query = $filtered_query . "gender = 'OTHER'";
+		$addons = $addons + 1;
+	}
+	$current_year = (int) date("Y");
+	$days = date("m-d");
+	if (isset($_POST['showOlderThan'])) {
+		if ($addons > 0) {
+			$filtered_query = $filtered_query . ' AND ';
+		}
+		$addons = $addons + 1;
+		$age_min = (int) $_POST['showOlderThan'];
+		$year_max = $current_year - $age_min;
+		$filtered_query = $filtered_query . "dateOfBirth < '{$year_max}-{$days}'";
+	}
+	if (isset($_POST['showYoungerThan'])) {
+		if ($addons > 0) {
+			$filtered_query = $filtered_query . ' AND ';
+		}
+		$addons = $addons + 1;
+		$age_max = (int) $_POST['showYoungerThan'];
+		$year_min = $current_year - $age_min;
+		$filtered_query = $filtered_query . "dateOfBirth > '{$year_max}-{$days}''";
+	}
+	return $filtered_query . ';';
+}
 ?>
--- login.php
+++ login.php
@@ -1,54 +1,49 @@
 <?php
 require('basic_functions.php');
 session_start();
-if (isLoggedIn())
-{
+if (isLoggedIn()) {
 	// redirect to feed
-	if ($_SESSION['isAdmin'])
-	{
+	if ($_SESSION['isAdmin']) {
 		header("Location: admin.php");
+	} else {
+		header("Location: feed2.html");
 	}
-	else
-	{
-		header("Location: feed.php");
-	}
-	exit(); // redirects to the feed or admin page
-}
-else
-{
+	exit(); 
+	// redirects to the feed or admin page
+} else {
 	// not logged in
-	if (wasPosted(array('email', 'password'))
-	{
+	if (wasPosted(array('email', 'password'))) {
 		// if credentials were posted
 		$email = $_POST['email'];
-		$password = $_POST['password'];
+		$password = md5($_POST['password']);
 		$conn = setupMySQL();
-		$sql = "SELECT * FROM Credentials WHERE email = '{$email}' AND password = '{$password}' LIMIT 1;";
+		
+		$sql = "SELECT * FROM Credentials WHERE email = '".$email."' AND password = '".$password."' LIMIT 1";
 		$result = $conn->query($sql);
 		$row = $result->fetch_assoc(); 
-		if ($row)
-		{
+		if ($row) {
 			$_SESSION['logged_in'] = true;
 			$_SESSION['user_id'] = $row['userId'];
 			$_SESSION['is_admin'] = $row['isAdmin'];
 			
-			header("Location: feed.php");
-			
-			if (! $row['isAdmin'])
-			{
-				// for normal user
-				setcookie("filter", "NONE", time() + 3600);
-				setcookie("gender", $row['gender'], time() + 3600);
-				setcookie("preferredGender", $row['preferredGender'], time() + 3600);
-				header("Location: feed.php");
+			if (! $row['isAdmin']) {
+				$sql_2 = "SELECT * FROM Users WHERE userId = {$_SESSION['userId']} LIMIT 1;";
+				$result_2 = $conn->query($sql_2);
+
+				// for normal user load that users details into cookies
+				setcookie("filter", "BEST_MATCH", time() + 7200);
+				setcookie("gender", $row['gender'], time() + 7200);
+				setcookie("gender_preference", $row['genderPreference'], time() + 7200);
+				header("Location: feed2.html");
+			} else {
+				header("Location: admin.php");
 			}
-			exit;
-		}
-		else
-		{
+		} else {
 			echo "Login failed, please check the credentials";
 		}
 		$conn->close();
+	} else {
+		header("Location: login.html");
 	}
 }
 ?>
